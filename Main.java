a546055b7b5ef406d9272d03b811ace37b486219
Rebecca Hufkie <G22H2807@ict.ru.ac.za>
2024-05-20 15:34:41 +0200

    Added feature to only output user-selected fields

--- Main.java
+++ Main.java
@@ -1,24 +1,51 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Specify the file name (or path if it's not in the same directory as the program)
+        // Specify the XML file name
         String fileName = "data.xml";
 
-        // Use try-with-resources to ensure the file is closed after reading
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            // Read each line from the file until the end
-            while ((line = br.readLine()) != null) {
-                // Print the line
-                System.out.println(line);
+        // Get user input for the desired field
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the field name to extract: ");
+        String fieldName = scanner.nextLine();
+        scanner.close();
+
+        // Parse the XML file and extract the desired fields
+        try {
+            // Create a DocumentBuilderFactory
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(fileName));
+
+            // Normalize the document
+            document.getDocumentElement().normalize();
+
+            // Get the root element
+            Element root = document.getDocumentElement();
+
+            // Get a list of all elements with the specified tag name
+            NodeList nodeList = root.getElementsByTagName(fieldName);
+
+            // Iterate over the elements and print their values
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                System.out.println(node.getTextContent());
             }
-        } catch (IOException e) {
-            // Handle potential IOException
-            System.err.println("Error reading the file: " + e.getMessage());
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
         }
     }
 }
+
 
