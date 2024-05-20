c5aa53505cb313b4a9d1db515c654bff3e251fcf
Rebecca Hufkie <G22H2807@ict.ru.ac.za>
2024-05-20 15:49:53 +0200

    Switched to using SAX XML parser

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,7 +19,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the field name to extract: ");
         String fieldName = scanner.nextLine().trim();
-        
+
         // Ensure the field name is not empty
         if (fieldName.isEmpty()) {
             System.out.println("Field name cannot be empty. Please provide a valid field name.");
@@ -32,55 +31,78 @@ public class Main {
 
         // Parse the XML file and extract the desired fields
         try {
-            // Create a DocumentBuilderFactory
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(new File(fileName));
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            UserFieldHandler handler = new UserFieldHandler(fieldName);
 
-            // Normalize the XML structure
-            document.getDocumentElement().normalize();
-
-            // Get the list of all <record> elements
-            NodeList recordList = document.getElementsByTagName("record");
-
-            // Create a JSONArray to store the extracted records
-            JSONArray jsonArray = new JSONArray();
-
-            // Iterate over each <record> element
-            for (int i = 0; i < recordList.getLength(); i++) {
-                Node recordNode = recordList.item(i);
-
-                if (recordNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element recordElement = (Element) recordNode;
-
-                    // Get the element by tag name
-                    NodeList fieldNodes = recordElement.getElementsByTagName(fieldName);
-
-                    // Check if the field exists
-                    if (fieldNodes.getLength() > 0) {
-                        JSONObject jsonObject = new JSONObject();
-                        for (int j = 0; j < fieldNodes.getLength(); j++) {
-                            jsonObject.put(fieldName + (j + 1), fieldNodes.item(j).getTextContent());
-                        }
-                        jsonArray.put(jsonObject);
-                    } else {
-                        System.out.println("Field '" + fieldName + "' not found in record " + (i + 1));
-                    }
-                }
-            }
+            saxParser.parse(new File(fileName), handler);
 
             // Check if any records were found
-            if (jsonArray.length() > 0) {
+            if (handler.getJsonArray().length() > 0) {
                 // Print the JSONArray in JSON format
-                System.out.println(jsonArray.toString(4)); // Indent with 4 spaces for readability
+                System.out.println(handler.getJsonArray().toString(4)); // Indent with 4 spaces for readability
             } else {
                 System.out.println("No records found for the field: " + fieldName);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("The file '" + fileName + "' was not found.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while accessing the file: " + e.getMessage());
+        } catch (SAXException e) {
+            System.out.println("An error occurred while parsing the XML file: " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("An error occurred while processing the XML file: " + e.getMessage());
+            System.out.println("An unexpected error occurred: " + e.getMessage());
             e.printStackTrace();
+        }
+    }
+}
+
+class UserFieldHandler extends DefaultHandler {
+    private String fieldName;
+    private JSONArray jsonArray;
+    private JSONObject currentRecord;
+    private StringBuilder currentValue;
+    private boolean isTargetField;
+    private int fieldCount;
+
+    public UserFieldHandler(String fieldName) {
+        this.fieldName = fieldName;
+        this.jsonArray = new JSONArray();
+        this.currentValue = new StringBuilder();
+        this.isTargetField = false;
+        this.fieldCount = 0;
+    }
+
+    public JSONArray getJsonArray() {
+        return jsonArray;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        currentValue.setLength(0);
+        if (qName.equals("record")) {
+            currentRecord = new JSONObject();
+            fieldCount = 0;
+        } else if (qName.equals(fieldName)) {
+            isTargetField = true;
+            fieldCount++;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        if (isTargetField) {
+            currentValue.append(ch, start, length);
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (qName.equals(fieldName) && isTargetField) {
+            currentRecord.put(fieldName + fieldCount, currentValue.toString().trim());
+            isTargetField = false;
+        } else if (qName.equals("record")) {
+            if (currentRecord.length() > 0) {
+                jsonArray.put(currentRecord);
+            }
         }
     }
 }
